9083be92f8fba9640e87761ef634485ffb98705b
Sigrid <hophfg@gmail.com>
2022-12-02 05:12:59 +0900

    finish till On road operators

--- x/tollroad/keeper/msg_server_road_operator.go
+++ x/tollroad/keeper/msg_server_road_operator.go
@@ -2,6 +2,8 @@ package keeper
 
 import (
 	"context"
+	"fmt"
+	"strconv"
 
 	"github.com/b9lab/toll-road/x/tollroad/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -11,28 +13,53 @@ import (
 func (k msgServer) CreateRoadOperator(goCtx context.Context, msg *types.MsgCreateRoadOperator) (*types.MsgCreateRoadOperatorResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
+	systemInfo, _ := k.GetSystemInfo(ctx)
+
 	// Check if the value already exists
 	_, isFound := k.GetRoadOperator(
 		ctx,
-		"0",
+		strconv.FormatUint(systemInfo.NextOperatorId, 10),
 	)
 	if isFound {
 		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "index already set")
 	}
 
+	nowOperatorId := strconv.FormatUint(systemInfo.NextOperatorId, 10)
+
+	// increment the next operator id
+	systemInfo.NextOperatorId++
+	k.SetSystemInfo(ctx, systemInfo)
+
+	fmt.Println("systemInfo", nowOperatorId)
+	fmt.Println("new system info ", systemInfo.NextOperatorId)
+
 	var roadOperator = types.RoadOperator{
 		Creator: msg.Creator,
-		Index:   "0",
+		Index:   nowOperatorId,
 		Name:    msg.Name,
 		Token:   msg.Token,
 		Active:  msg.Active,
 	}
 
+	// emit new event
+	ctx.EventManager().EmitEvents(sdk.Events{
+		sdk.NewEvent(
+			"new-road-operator-created",
+			sdk.NewAttribute("creator", "A"),
+			sdk.NewAttribute("road-operator-index", nowOperatorId),
+			sdk.NewAttribute("name", msg.Name),
+			sdk.NewAttribute("token", msg.Token),
+			sdk.NewAttribute("active", "true"),
+		),
+	})
+
 	k.SetRoadOperator(
 		ctx,
 		roadOperator,
 	)
-	return &types.MsgCreateRoadOperatorResponse{}, nil
+	return &types.MsgCreateRoadOperatorResponse{
+		Index: nowOperatorId,
+	}, nil
 }
 
 func (k msgServer) UpdateRoadOperator(goCtx context.Context, msg *types.MsgUpdateRoadOperator) (*types.MsgUpdateRoadOperatorResponse, error) {
--- x/tollroad/types/genesis.go
+++ x/tollroad/types/genesis.go
@@ -10,7 +10,9 @@ const DefaultIndex uint64 = 1
 // DefaultGenesis returns the default Capability genesis state
 func DefaultGenesis() *GenesisState {
 	return &GenesisState{
-		SystemInfo:       SystemInfo{},
+		SystemInfo: SystemInfo{
+			NextOperatorId: 1,
+		},
 		RoadOperatorList: []RoadOperator{},
 		// this line is used by starport scaffolding # genesis/types/default
 		Params: DefaultParams(),
